3f3d0796d2e706bea40dc3564cfd14957bff46ee
George <71434803+george-swift@users.noreply.github.com>
2024-12-15 13:08:21 +0100

    Update import resolution paths in tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,9 +1,11 @@
-from src.utils.exceptions import UnauthorizedError
+from src.exceptions import UnauthorizedError
+
 
 def test_unauthorized_error_default_message():
     """Test that UnauthorizedError has the default message."""
     error = UnauthorizedError()
     assert str(error) == "Unauthorized"
+
 
 def test_unauthorized_error_custom_message():
     """Test that UnauthorizedError can accept a custom message."""
